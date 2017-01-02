b91e34407f1cbcd07007f616f505ade4cc6c2c94
Julien Ronck <j.ronck@decisit.com>
2017-01-02 18:45:06 +0100

    Groups/Users

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,11 +7,21 @@ class GroupsController < ApplicationController
   def create
     group = Group.new(group_params)
     group.save
-    redirect_to new_group_path
+    redirect_to edit_group_path(group)
+  end
+
+  def edit
+    @group = Group.find(params.require(:id))
+  end
+
+  def update
+    group = Group.find(params.require(:id))
+    group.update_attributes(group_params)
+    redirect_to edit_group_path(group)
   end
 
   private
   def group_params
-    params.require(:group).permit(:name, users_attributes: [:name])
+    params.require(:group).permit(:name, users_attributes: [:id, :name])
   end
 end
